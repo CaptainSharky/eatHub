589f489ab059a193ba7e2b1b8d33a26c630a9649
Kirill Prokofyev <kprokofyev@whoosh.bike>
2025-03-25 16:51:40 +0300

    ETH-7: Завел проект

---
+++ EatHub/EatHub/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  EatHub
+//
+//  Created by Kirill Prokofyev on 25.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("Fetch") { }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
